bc099643dbfc87072b2e6839aaaa63a6fbec8ee3
Pandamentals <t95njy2w7k@privaterelay.appleid.com>
2026-03-27 21:04:26 -0400

    V1

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = "timecard-v1";
+const FILES = [
+  "./index.html",
+  "./manifest.json",
+  "https://fonts.googleapis.com/css2?family=VT323&family=Share+Tech+Mono&display=swap"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
